9ca9519484878c4bb36b34b4111eca4b80acfdd8
Adam Szkoda <adam.szkoda@fieldaware.com>
2017-09-18 16:05:17 +0200

    Remap slash to OSM(MOD_RALT) for Polish diacritics

--- keyboards/tada68/keymaps/default/keymap.c
+++ keyboards/tada68/keymaps/default/keymap.c
@@ -32,16 +32,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 KC_GESC,        KC_1,       KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,       KC_9,           KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,  KC_GRV, \
 KC_TAB,         KC_Q,       KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,       KC_O,           KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,  KC_DEL, \
 KC_LCTL,        KC_A,       KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,       KC_L,           KC_SCLN,    KC_QUOT,                KC_ENT,   KC_PGUP,  \
-OSM(MOD_LSFT),  KC_Z,       KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,    KC_DOT,         KC_SLSH,    KC_RSHIFT,              KC_UP,    KC_PGDN, \
+OSM(MOD_LSFT),  KC_Z,       KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,    KC_DOT,         OSM(MOD_RALT),    KC_RSHIFT,              KC_UP,    KC_PGDN, \
 MO(_FL),        KC_LALT,    KC_LGUI,                KC_SPC,                             KC_RALT,        TG(_FL),    OSM(MOD_LSFT | MOD_LALT | MOD_LGUI),      KC_LEFT,    KC_DOWN,  KC_RGHT),
 
 
 /* _FL: Function Layer */
 [_FL] = KEYMAP_ANSI(
 _______, KC_F1,     KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     KC_DEL,     KC_POWER ,  \
-_______, _______,   _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_INS, \
-_______, _______,   _______,    _______,    _______,    _______,    KC_LEFT,    KC_DOWN,    KC_UP,      KC_RGHT,    _______,    _______,    _______,    KC_HOME, \
-_______, _______,   _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_PGUP,    KC_END, \
+_______, _______,   KC_VOLU,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_INS, \
+_______, BL_DEC,    KC_VOLD,    BL_INC,     _______,    _______,    KC_LEFT,    KC_DOWN,    KC_UP,      KC_RGHT,    _______,    _______,    _______,    KC_HOME, \
+_______, _______,   _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_SLSH,    _______,    KC_PGUP,    KC_END, \
 _______, _______,   _______,                _______,                _______,    _______,    _______,    KC_HOME,    KC_PGDN,    KC_END),
 
 
