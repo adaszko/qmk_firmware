1b77db736976cbb204d2d465c9c2a3aa06ee52c9
Adam Szkoda <adam.szkoda@fieldaware.com>
2018-01-26 08:30:55 +0100

    Add macros for brackets

--- keyboards/tada68/keymaps/default/keymap.c
+++ keyboards/tada68/keymaps/default/keymap.c
@@ -16,7 +16,9 @@
 
 
 enum custom_keycodes {
-    MACRO1 = SAFE_RANGE
+    M_PARENS = SAFE_RANGE,
+    M_SQBR,
+    M_CRBR
 };
 
 
@@ -29,35 +31,43 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* _BL: (Base Layer) Default Layer */
 [_BL] = KEYMAP_ANSI(
-KC_GESC,        KC_1,       KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,       KC_9,           KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,  KC_GRV, \
-KC_TAB,         KC_Q,       KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,       KC_O,           KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,  KC_DEL, \
-KC_LCTL,        KC_A,       KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,       KC_L,           KC_SCLN,    KC_QUOT,                KC_ENT,   KC_PGUP,  \
-OSM(MOD_LSFT),  KC_Z,       KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,    KC_DOT,         OSM(MOD_RALT),    KC_RSHIFT,              KC_UP,    KC_PGDN, \
-MO(_FL),        KC_LALT,    KC_LGUI,                KC_SPC,                             KC_RALT,        TG(_FL),    OSM(MOD_LSFT | MOD_LALT | MOD_LGUI),      KC_LEFT,    KC_DOWN,  KC_RGHT),
+KC_GESC,        KC_1,       KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,       KC_9,           KC_0,       KC_MINS,    KC_EQL,         KC_BSPC,  KC_GRV, \
+KC_TAB,         KC_Q,       KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,       KC_O,           KC_P,       KC_LBRC,    KC_RBRC,        KC_BSLS,  KC_DEL, \
+KC_LCTL,        KC_A,       KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,       KC_L,           KC_SCLN,    KC_QUOT,                    KC_ENT,   KC_PGUP,  \
+KC_LSHIFT,      KC_Z,       KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,    KC_DOT,         KC_SLSH,    KC_RSHIFT,                  KC_UP,    KC_PGDN, \
+MO(_FL),        KC_LALT,    KC_LGUI,                KC_SPC,                             KC_RALT,        MO(_FL),    KC_HYPR,    KC_LEFT,        KC_DOWN,  KC_RGHT),
 
 
 /* _FL: Function Layer */
 [_FL] = KEYMAP_ANSI(
-_______, KC_F1,     KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     KC_DEL,     KC_POWER ,  \
+_______, KC_F1,     KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     KC_DEL,     KC_POWER,  \
 _______, _______,   KC_VOLU,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_INS, \
-_______, BL_DEC,    KC_VOLD,    BL_INC,     _______,    _______,    KC_LEFT,    KC_DOWN,    KC_UP,      KC_RGHT,    _______,    _______,    _______,    KC_HOME, \
-_______, _______,   _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_SLSH,    _______,    KC_PGUP,    KC_END, \
-_______, _______,   _______,                _______,                _______,    _______,    _______,    KC_HOME,    KC_PGDN,    KC_END),
+_______, BL_DEC,    KC_VOLD,    BL_INC,     _______,    _______,    _______,    M_PARENS,   M_SQBR,     M_CRBR,     _______,    _______,                _______,    KC_HOME, \
+_______, _______,   _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,                KC_PGUP,    KC_END, \
+_______, _______,   _______,                _______,                                                    _______,    _______,    _______,    KC_HOME,    KC_PGDN,    KC_END),
 
 
 };
 
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-#if 0
     if (record->event.pressed) {
         switch(keycode) {
-            case MACRO1:
-                SEND_STRING("");
+            case M_PARENS:
+                SEND_STRING("()" SS_TAP(X_LEFT));
+                return false;
+                break;
+
+            case M_SQBR:
+                SEND_STRING("[]" SS_TAP(X_LEFT));
+                return false;
+                break;
+
+            case M_CRBR:
+                SEND_STRING("{}" SS_TAP(X_LEFT));
                 return false;
                 break;
         }
     }
-#endif
     return true;
 }
