da3d4fe7b4fee9fa79639b695761f3673a2e70b8
Adam Szkoda <adaszko@gmail.com>
2019-07-31 10:45:00 +0200

    Bind volume up and down to s and w on the second layer

--- keyboards/clueboard/66_hotswap/keymaps/default/keymap.c
+++ keyboards/clueboard/66_hotswap/keymaps/default/keymap.c
@@ -38,15 +38,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,  KC_BSLS,        KC_PGDN, \
   KC_LCTL,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,          KC_ENT, \
   KC_LSFT,        KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,        KC_UP, \
-  MO(_FL),KC_LALT,KC_LGUI,                KC_SPC, KC_SPC,                         KC_RGUI,KC_RALT,MO(_FL),  KC_RCTL,KC_LEFT,KC_DOWN,KC_RGHT),
+  MO(_FL),KC_LALT,KC_LGUI,                KC_SPC, KC_SPC,                         _______,KC_RALT,KC_RGUI,  MO(_CL),KC_LEFT,KC_DOWN,KC_RGHT),
 
   /* Keymap _FL: Function Layer
    */
 [_FL] = LAYOUT(
-  KC_GRV, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12,      KC_DEL,            KC_VOLU, \
-  _______,_______,_______,_______,_______,_______,_______,_______,_______,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_MUTE,                KC_VOLD, \
-  _______,_______,MO(_CL),_______,_______,_______,_______,_______,_______,_______,_______,_______,    _______,                             \
-  _______,        _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,        _______,        KC_PGUP,         \
+  KC_GRV, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_DEL,           KC_VOLU, \
+  _______,_______,KC_VOLU,_______,_______,_______,_______,_______,_______,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_MUTE,          KC_VOLD, \
+  _______,_______,KC_VOLD,_______,_______,_______,_______,_______,_______,_______,_______,_______,        _______, \
+  _______,        _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,        _______,        KC_PGUP, \
   _______,_______,_______,                _______,_______,                        _______,_______,MO(_FL),_______,KC_HOME,KC_PGDN,KC_END),
 
   /* Keymap _CL: Control layer
