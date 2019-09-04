93236f7141bc0144b7b1b5afa7c3b23674f74d85
Adam Szkoda <adaszko@gmail.com>
2019-09-04 14:23:40 +0200

    Map insert key for pasting in Alacritty

--- keyboards/clueboard/66_hotswap/keymaps/default/keymap.c
+++ keyboards/clueboard/66_hotswap/keymaps/default/keymap.c
@@ -19,32 +19,32 @@ enum custom_keycodes {
 };
 
 #ifdef AUDIO_ENABLE
-  float song_basketcase[][2] = SONG(BASKET_CASE);
-  float song_ode_to_joy[][2]  = SONG(ODE_TO_JOY);
-  float song_rock_a_bye_baby[][2]  = SONG(ROCK_A_BYE_BABY);
-  float song_doe_a_deer[][2]  = SONG(DOE_A_DEER);
-  float song_scale[][2]  = SONG(MUSIC_SCALE_SOUND);
-  float song_coin[][2]  = SONG(COIN_SOUND);
-  float song_one_up[][2]  = SONG(ONE_UP_SOUND);
-  float song_sonic_ring[][2]  = SONG(SONIC_RING);
-  float song_zelda_puzzle[][2]  = SONG(ZELDA_PUZZLE);
+  float song_basketcase[][2]        = SONG(BASKET_CASE);
+  float song_ode_to_joy[][2]        = SONG(ODE_TO_JOY);
+  float song_rock_a_bye_baby[][2]   = SONG(ROCK_A_BYE_BABY);
+  float song_doe_a_deer[][2]        = SONG(DOE_A_DEER);
+  float song_scale[][2]             = SONG(MUSIC_SCALE_SOUND);
+  float song_coin[][2]              = SONG(COIN_SOUND);
+  float song_one_up[][2]            = SONG(ONE_UP_SOUND);
+  float song_sonic_ring[][2]        = SONG(SONIC_RING);
+  float song_zelda_puzzle[][2]      = SONG(ZELDA_PUZZLE);
 #endif
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap _BL: Base Layer (Default Layer)
    */
 [_BL] = LAYOUT(
-  KC_GESC,KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL,   KC_BSPC,        KC_PGUP, \
-  KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,  KC_BSLS,        KC_PGDN, \
+  KC_GESC,KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL,   KC_BSPC,        KC_INSERT, \
+  KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,  KC_BSLS,        KC_SYSREQ, \
   KC_LCTL,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,          KC_ENT, \
-  KC_LSFT,        KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,        KC_UP, \
+  KC_LSPO,        KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSPC,        KC_UP, \
   MO(_FL),KC_LALT,KC_LGUI,                KC_SPC, KC_SPC,                         _______,KC_RALT,KC_RGUI,  MO(_CL),KC_LEFT,KC_DOWN,KC_RGHT),
 
   /* Keymap _FL: Function Layer
    */
 [_FL] = LAYOUT(
-  KC_GRV, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_DEL,           KC_VOLU, \
-  _______,_______,KC_VOLU,_______,_______,_______,_______,_______,_______,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_MUTE,          KC_VOLD, \
+  KC_GRV, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_DEL,           KC_WAKE, \
+  _______,_______,KC_VOLU,_______,_______,_______,_______,_______,_______,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_MUTE,          KC_SLEP, \
   _______,_______,KC_VOLD,_______,_______,_______,_______,_______,_______,_______,_______,_______,        _______, \
   _______,        _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,        _______,        KC_PGUP, \
   _______,_______,_______,                _______,_______,                        _______,_______,MO(_FL),_______,KC_HOME,KC_PGDN,KC_END),
